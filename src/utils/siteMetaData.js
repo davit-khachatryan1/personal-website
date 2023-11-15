14bcf4003288a683e533bc874972ba71367ee764
davit-khachatryan1 <khachatryandavit55@gmail.com>
2023-11-15 21:24:40 +0400

    fully version

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,22 @@
+// please update required information
+const siteMetadata = {
+  title: "Next.js Blog With Tailwind CSS and Content layer",
+  author: "I",
+  headerTitle: "Next.js Blog",
+  description: "A blog created with Next.js, Tailwind.css and content layer.",
+  language: "en-us",
+  theme: "system",
+  siteUrl: "https://www.upwork.com/freelancers/davitk10",
+  siteLogo: "/logo.png",
+  socialBanner: "/social-banner.png",
+  email: "khachatryandavit55@gmail.com",
+  github: "https://github.com/",
+  twitter: "https://twitter.com/",
+  facebook: "https://facebook.com",
+  youtube: "https://youtube.com/",
+  linkedin: "https://www.linkedin.com/in/",
+  dribbble: "https://www.dribbble.com",
+  locale: "en-US",
+};
+
+module.exports = siteMetadata;
